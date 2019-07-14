e74e18ba17ffa008892eb16936d6acc4f5d129f1
Magdalena Dusza <magdalena.dusza13@gmail.com>
2019-07-14 22:14:13 +0200

    test old app

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,5 @@
-const config = require("config.json");
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || config.connectionString);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test");
 mongoose.Promise = global.Promise;
 
 module.exports = {
--- server.js
+++ server.js
@@ -25,19 +25,19 @@ app.use(errorHandler);
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-});
+// });
